4c1b0845a84e0ba99491bae0a3e9321fdd02c231
hoonii2 <lkh66913@naver.com>
2023-07-15 23:01:16 +0900

    fix #2 : Fix member entity field and auditing

--- src/main/java/com/example/cleansnsmodeling/domain/member/entity/Member.java
+++ src/main/java/com/example/cleansnsmodeling/domain/member/entity/Member.java
@@ -1,15 +1,24 @@
 package com.example.cleansnsmodeling.domain.member.entity;
 
+import com.example.cleansnsmodeling.domain.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "member")
-public class Member {
+public class Member extends BaseEntity {
+
+    @Transient
+    static final int MAX_NICKNAME_LENGTH = 10;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,12 +26,21 @@ public class Member {
     private Long id;
 
     @Column(name = "nickname")
+    @Length(max = 10, message = "회원 이름은 최대 10글자")
     private String nickname;
 
     @Column(name = "birthday")
     private LocalDate birthday;
 
-    @Column(name = "createdAt")
-    private LocalDateTime createdAt;
+    @Builder
+    public Member(Long id, String nickname, LocalDate birthday) {
+        this.id = id;
+        this.nickname = nickname;
+        this.birthday = birthday;
+    }
 
+    public void updateNickname(String toNickname){
+        Objects.requireNonNull(toNickname);
+        nickname = toNickname;
+    }
 }
